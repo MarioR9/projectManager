9b4c2e9ffafafff3e7f38dcbf2611a9a5ebb0085
marior9 <mariorodriguezan@gmail.com>
2020-08-13 20:59:34 -0400

    Project domain class added

---
+++ grails-app/domain/pm/api/Project.groovy
@@ -0,0 +1,12 @@
+package pm.api
+
+class Project {
+
+    String name
+    String description
+    String startDate
+    String endDate
+    
+    static constraints = {
+    }
+}
--- grails-app/init/pm/api/BootStrap.groovy
+++ grails-app/init/pm/api/BootStrap.groovy
@@ -3,6 +3,12 @@ package pm.api
 class BootStrap {
 
     def init = { servletContext ->
+        new Project(
+                    name: "TranslateMe Mobile",
+                    description: "Object recognition to translate objects using phone's camera",
+                    startDate: "August",
+                    endDate: "September").save()
+                    
     }
     def destroy = {
     }
